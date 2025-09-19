eadc8bd11230acfa45e805d475b6e6726ab9f1c6
Scott Bissell <minewhip@gmail.com>
2025-09-18 21:02:06 -0700

    Added *really* basic cli tools

--- IPT.go
+++ IPT.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	Database "IPT/Imbedded-Pixel-Tracker/database"
 	"IPT/Imbedded-Pixel-Tracker/router"
 	"IPT/Imbedded-Pixel-Tracker/routes"
 	"IPT/Imbedded-Pixel-Tracker/routes_client"
 	"IPT/Imbedded-Pixel-Tracker/routes_server"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -28,5 +34,73 @@ func main() {
 	routes_client.Client_Routes(r)
 	routes_server.Server_Routes(r)
 
+	go command_line_interface()
 	router.RunRouter(r)
+
+}
+
+// IK all the cli code is garbage I added it last second if you want feel free to make it better
+func command_line_interface() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		print(">")
+		var command string
+		var clientID string
+
+		input, _ := reader.ReadString('\n')
+		input = strings.Split(input, "\n")[0]
+
+		//n, err := fmt.Scan(&input)
+		//log.Print(n, err)
+		log.Println(input)
+
+		input_list := strings.Split(input, " ")
+		command = input_list[0]
+		if len(strings.Split(input, " ")) > 1 {
+			clientID = input_list[1]
+		}
+
+		switch strings.ToLower(command) {
+		case "help":
+			fmt.Println("\nCommands are:")
+			fmt.Println("Create [ClientID]")
+			fmt.Println("Status [ClientID]")
+			fmt.Println("Delete [ClientID]")
+		case "create":
+			url, err := create_tracker(clientID)
+			if err != nil {
+				fmt.Println("\nCommand Failed because:", err)
+			} else {
+				fmt.Println("Tracker URL:", url)
+			}
+		case "status":
+			tracker, err := tracker_status(clientID)
+			if err != nil {
+				fmt.Println("\nCommand Failed because:", err)
+			} else {
+				fmt.Println(tracker)
+			}
+		case "delete":
+			err := delete_tracker(clientID)
+			if err != nil {
+				fmt.Println("\nCommand Failed because:", err)
+			}
+		}
+	}
+}
+
+func create_tracker(clientID string) (string, error) {
+	url, err := Database.Generate_Tracker(clientID)
+
+	return url, err
+}
+
+func tracker_status(clientID string) (Database.Tracker, error) {
+	tracker, err := Database.Tracker_Status(clientID)
+	return tracker, err
+}
+
+func delete_tracker(clientID string) error {
+	err := Database.Remove_Tracker(clientID)
+	return err
 }
